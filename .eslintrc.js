a3a99aa10be3fb146b4e7e3b9ff2bb4b1f8fcc39
Fahim <fracturx@outlook.com>
2020-08-07 02:48:48 -0400

    eslint configuartion

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2020': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 11
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
