24699bd278c024cf2eaf91caab8fecaaff6f7c3e
Fahim <fracturx@outlook.com>
2020-08-07 03:21:19 -0400

    revised eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,41 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'commonjs': true,
-		'es2020': true,
-		'node': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended'
-	],
-	'parserOptions': {
-		'ecmaFeatures': {
-			'jsx': true
-		},
-		'ecmaVersion': 11
-	},
-	'plugins': [
-		'react'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    indent: [
+      'error',
+      2,
+    ],
+    quotes: [
+      'error',
+      'single',
+    ],
+    semi: [
+      'error',
+      'always',
+    ],
+    'linebreak-style': 'off',
+    'class-methods-use-this': 'off',
+    'import/extensions': 'off',
+    'no-console': ['error'],
+    'import/prefer-default-export': 'off',
+  },
 };
