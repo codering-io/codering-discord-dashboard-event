caf007ee23838324d52a650567e4a4c82a76ccc4
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-08-10 12:09:56 -0400

    change var names

--- server/src/database/models/User.js
+++ server/src/database/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const Guild = new mongoose.Schema({
+const User = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
   guilds: { type: Array, required: true },
   discordTag: { type: String, required: true },
 });
-module.exports = mongoose.model('guilds', Guild);
+module.exports = mongoose.model('users', User);
