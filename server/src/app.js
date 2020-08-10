8451f955557302b39757cdeda7657b9dd7e9ecd9
Fahim <fracturx@outlook.com>
2020-08-10 16:35:24 -0400

    Event Project Configuration

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,18 @@
 /* eslint-disable no-console */
 require('dotenv').config();
 require('./database/database');
+const passport = require('passport');
+const { Strategy } = require('passport-discord');
 
-const passport = require("./strategy");
+// const Guild = require('./database/models/Guild');
+// const User = require('./database/models/User');
+
+passport.use(new Strategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.CLIENT_CALLBACK_URL,
+  scope: ['identify', 'guilds'],
+}));
 
 const express = require('express');
 const passport = require('./strategy');
--- server/src/database/models/User.js
+++ server/src/database/models/User.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-  guilds: [{ type: mongoose.Types.ObjectId, ref: "guilds", required: true }],
+  guilds: [{ type: mongoose.Types.ObjectId, ref: 'guilds', required: true }],
   userId: { type: String, required: true },
-  discordTag: { type: String, required: true }
+  discordTag: { type: String, required: true },
 });
-module.exports = mongoose.model("users", User);
+module.exports = mongoose.model('users', User);
