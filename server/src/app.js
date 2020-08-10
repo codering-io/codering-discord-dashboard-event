8061b8c1abd7c3ce7b7e21f4b66c63876cfc462b
computergeek12 <61062461+computergeek12@users.noreply.github.com>
2020-08-10 15:16:31 -0500

    Add Passport/DiscordStrategy

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,19 @@
 require('dotenv').config();
 require('./database/database');
 
+const Guild = require("./database/models/Guild");
+const User = require("./database/models/User");
+
+const passport = require("passport");
+const { Strategy } = require("passport-discord");
+
+passport.use(new Strategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.CLIENT_CALLBACK_URL,
+  scope: ["identify", "guilds"]
+}));
+
 const express = require('express');
 
 const app = express();
