cf60ddbc3587e0d4c9c950283cd8c0e5b2f7a42a
computergeek12 <61062461+computergeek12@users.noreply.github.com>
2020-08-10 14:54:55 -0500

    Merge pull request #14 from computergeek12/master

---
+++ server/src/database/models/User.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const User = new mongoose.Schema({
+  guilds: [{ type: mongoose.Types.ObjectId, ref: "guilds", required: true }],
+  userId: { type: String, required: true },
+  discordTag: { type: String, required: true }
+});
+module.exports = mongoose.model("users", User);
