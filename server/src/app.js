80e7f810f5d2fbe5dce549696bf3e4a7256267ea
computergeek12 <61062461+computergeek12@users.noreply.github.com>
2020-08-10 16:02:12 -0500

    Passport
    - Forgot to add app.use()
    - Added callback that updates/adds user data

--- server/src/app.js
+++ server/src/app.js
@@ -8,17 +8,67 @@ const User = require("./database/models/User");
 const passport = require("passport");
 const { Strategy } = require("passport-discord");
 
+const { Client, Permissions } = require('discord.js');
+const client = new Client();
+client.login(process.env.BOT_TOKEN);
+
 passport.use(new Strategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   callbackURL: process.env.CLIENT_CALLBACK_URL,
   scope: ["identify", "guilds"]
+}, async (accessToken, refreshToken, profile, done) => {
+  var user = await User.findOne({ userId: profile.id });
+  if (!user) {
+    var userGuilds = profile.guilds.filter(g => {
+      var permissions = new Permissions(g.permissions);
+      if (permissions.has("MANAGE_GUILD") && client.guilds.cache.has(g.id)) return true;
+      return false;
+    });
+    var objIds = [];
+    for (var g of userGuilds) {
+      var guild = await Guild.findOne({ guildId: g.id });
+      if (!guild) {
+        guild = (new Guild({
+          guildId: g.id,
+          prefix: "!"
+        })).save();
+      }
+      objIds.push(guild.id);
+    }
+    user = (new User({
+      userId: profile.id,
+      discordTag: `${profile.username}#${profile.discriminator}`,
+      guilds: objIds
+    })).save();
+  } else {
+    var userGuilds = profile.guilds.filter(g => {
+      var permissions = new Permissions(g.permissions);
+      if (permissions.has("MANAGE_GUILD") && client.guilds.cache.has(g.id)) return true;
+      return false;
+    });
+    var objIds = [];
+    for (var g of userGuilds) {
+      var guild = await Guild.findOne({ guildId: g.id });
+      if (!guild) {
+        guild = (new Guild({
+          guildId: g.id,
+          prefix: "!"
+        })).save();
+      }
+      objIds.push(guild.id);
+    }
+    User.findOneAndUpdate({ discordTag: `${profile.username}#${profile.discriminator}`, guilds: objIds });
+  }
 }));
 
 const express = require('express');
 
 const app = express();
 app.use(express.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
