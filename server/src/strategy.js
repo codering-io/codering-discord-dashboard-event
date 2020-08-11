a97835fccce58339c9a31640e24d6ad6ec3816a8
computergeek12 <61062461+computergeek12@users.noreply.github.com>
2020-08-10 19:48:28 -0500

    Add done function

--- server/src/strategy.js
+++ server/src/strategy.js
@@ -63,6 +63,7 @@ passport.use(new Strategy({
     }
     User.findOneAndUpdate({ discordTag: `${profile.username}#${profile.discriminator}`, guilds: objIds });
   }
+  done();
 }));
 
 module.exports = passport;
