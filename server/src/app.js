4ed1739bf11362e73798a56fe7d8a37de6c9a4fa
computergeek12 <61062461+computergeek12@users.noreply.github.com>
2020-08-11 06:21:31 -0500

    Task #3

--- server/src/app.js
+++ server/src/app.js
@@ -11,5 +11,14 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/api/auth/discord', passport.authenticate('discord'));
+
+app.get('/api/auth/discord/redirect', passport.authenticate('discord'), (req, res) => res.redirect('/dashboard'));
+
+app.get('/api/auth/', passport.authenticate('discord'), async (req, res) => {
+  if (!req.user) return res.sendStatus(401);
+  res.send({ guilds: req.user.guilds, userId: req.user.userId, discordTag: req.user.discordTag });
+});
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
