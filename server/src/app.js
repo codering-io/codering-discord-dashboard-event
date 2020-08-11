22cc4156ce8b9144c1ae0842964790534db86c68
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-08-11 14:15:44 -0400

    sessions should work now

--- server/src/app.js
+++ server/src/app.js
@@ -13,6 +13,16 @@ app.use(express.json());
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(
+  session({
+    secret: "Testing",
+    cookie: {
+      maxAge: 60 * 1000 * 60 * 24,
+    },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use('/api/auth/', authenticationRouter);
 
