0b4374cae718b1afce9b68d01e759c2cee850866
Fahim <fracturx@outlook.com>
2020-08-10 22:05:45 -0400

    test passport strategy temporary routes

--- server/src/app.js
+++ server/src/app.js
@@ -11,5 +11,14 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
+// for testing purposes
+
+// app.get('/api/auth/discord', passport.authenticate('discord'));
+
+/* app.get('/api/auth/discord/redirect', passport.authenticate('discord'), (req, res) => {
+  res.send(200);
+});
+*/
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
