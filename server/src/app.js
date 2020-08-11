860f3677f504f2408969d602f822492340db86cf
computergeek12 <61062461+computergeek12@users.noreply.github.com>
2020-08-11 06:21:31 -0500

    Create auth routes

--- server/src/app.js
+++ server/src/app.js
@@ -15,13 +15,18 @@ passport.use(new Strategy({
 }));
 
 const express = require('express');
+
 const passport = require('./strategy');
+
+const authenticationRouter = require('./routers/authentication');
 
 const app = express();
 app.use(express.json());
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use('/api/auth/', authenticationRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ server/src/routers/authentication.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+
+const router = Router();
+
+const passport = require('../strategy');
+
+router.get('/discord/', passport.authenticate('discord'));
+
+router.get('/discord/redirect/', passport.authenticate('discord'), (req, res) => res.redirect('/dashboard'));
+
+router.get('/', passport.authenticate('discord'), async (req, res) => {
+  if (!req.user) return res.sendStatus(401);
+  res.send({ guilds: req.user.guilds, userId: req.user.userId, discordTag: req.user.discordTag });
+});
+
+// eslint-disable-next-line eol-last
+module.exports = router;
