320b0244d9471dad4743fe20f54c35ce2fb13a86
computergeek12 <61062461+computergeek12@users.noreply.github.com>
2020-08-11 10:25:05 -0500

    Add prefix router

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ const express = require('express');
 const passport = require('./strategy');
 
 const authenticationRouter = require('./routers/authentication');
+const prefixRouter = require('./routers/prefix');
 
 const app = express();
 app.use(express.json());
@@ -15,6 +16,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/api/auth/', authenticationRouter);
+app.use('/api/prefix/', prefixRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
---
+++ server/src/routers/prefix.js
@@ -0,0 +1,22 @@
+/* eslint-disable eol-last */
+/* eslint-disable no-trailing-spaces */
+const { Router } = require('express');
+
+const router = Router();
+
+const Guild = require('../database/models/Guild');
+
+const passport = require('../strategy');
+
+router.get('/api/prefix/', passport.authenticate('discord'), async (req, res) => {
+  if (!req.user) return res.sendStatus(401);
+  const prefix = {};
+  for (const guild of req.user.guilds) {
+    // eslint-disable-next-line no-await-in-loop
+    const g = await Guild.findById(guild);
+    prefix[g.guildId] = g.prefix;
+  }
+  res.send(prefix);
+});
+
+module.exports = router; 
