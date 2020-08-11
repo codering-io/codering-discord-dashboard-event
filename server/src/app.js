ee7146a70d7b87c2d6f70b38766d8d634fc1fb3b
computergeek12 <61062461+computergeek12@users.noreply.github.com>
2020-08-11 13:53:56 -0500

    Add prefix router (#21)
    * Add prefix router
    
    * oops
    
    * update linting
    
    * Fix auth
    
    Co-authored-by: Creeper <51346607+CreeperPlanet26@users.noreply.github.com>

--- server/src/app.js
+++ server/src/app.js
@@ -3,30 +3,30 @@ require('dotenv').config();
 require('./database/database');
 
 const express = require('express');
-
 const session = require('express-session');
 
 const passport = require('./strategy');
 
 const authenticationRouter = require('./routers/authentication');
+const prefixRouter = require('./routers/prefix');
 
 const app = express();
 app.use(express.json());
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(
-  session({
-    secret: 'Testing',
-    cookie: {
-      maxAge: 60 * 1000 * 60 * 24,
-    },
-    resave: false,
-    saveUninitialized: false,
-  }),
-);
+
+app.use(session({
+  secret: 'Testing',
+  cookie: {
+    maxAge: 60 * 1000 * 60 * 24,
+  },
+  resave: false,
+  saveUninitialized: false,
+}));
 
 app.use('/api/auth/', authenticationRouter);
+app.use('/api/prefix/', prefixRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
