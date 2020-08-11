728c84296806d87452c52826c4b460e249a11b8b
CreeperPlanet26 <fortniteplanet26@gmail.com>
2020-08-11 14:15:44 -0400

    Event Project Maintenance

--- server/src/app.js
+++ server/src/app.js
@@ -16,6 +16,8 @@ passport.use(new Strategy({
 
 const express = require('express');
 
+const session = require('express-session');
+
 const passport = require('./strategy');
 
 const authenticationRouter = require('./routers/authentication');
@@ -25,6 +27,16 @@ app.use(express.json());
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(
+  session({
+    secret: 'Testing',
+    cookie: {
+      maxAge: 60 * 1000 * 60 * 24,
+    },
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 
 app.use('/api/auth/', authenticationRouter);
 
