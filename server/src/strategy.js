0cff34a4bb653556a06e016a27d4f0b322bde7d4
computergeek12 <61062461+computergeek12@users.noreply.github.com>
2020-08-10 20:03:18 -0500

    done

--- server/src/strategy.js
+++ server/src/strategy.js
@@ -11,59 +11,63 @@ passport.use(new Strategy({
   callbackURL: process.env.CLIENT_CALLBACK_URL,
   scope: ['identify', 'guilds'],
 }, async (accessToken, refreshToken, profile, done) => {
-  let user = await User.findOne({ userId: profile.id });
-  if (!user) {
-    const userGuilds = profile.guilds.filter(async (g) => {
-      const guilds = await axios.get(`https://discord.com/api/v6/users/${process.env.CLIENT_ID}/guilds`, {
-        headers: {
-          Authorization: `BOT ${process.env.BOT_TOKEN}`,
-        },
+  try {
+    let user = await User.findOne({ userId: profile.id });
+    if (!user) {
+      const userGuilds = profile.guilds.filter(async (g) => {
+        const guilds = await axios.get(`https://discord.com/api/v6/users/${process.env.CLIENT_ID}/guilds`, {
+          headers: {
+            Authorization: `BOT ${process.env.BOT_TOKEN}`,
+          },
+        });
+        if ((g.permissions & 32) === 32 && guilds.some((i) => i.id === g.id)) return true;
+        return false;
       });
-      if ((g.permissions & 32) === 32 && guilds.some((i) => i.id === g.id)) return true;
-      return false;
-    });
-    const objIds = [];
-    for (const g of userGuilds) {
-      // eslint-disable-next-line no-await-in-loop
-      let guild = await Guild.findOne({ guildId: g.id });
-      if (!guild) {
-        guild = (new Guild({
-          guildId: g.id,
-          prefix: '!',
-        })).save();
+      const objIds = [];
+      for (const g of userGuilds) {
+        // eslint-disable-next-line no-await-in-loop
+        let guild = await Guild.findOne({ guildId: g.id });
+        if (!guild) {
+          guild = (new Guild({
+            guildId: g.id,
+            prefix: '!',
+          })).save();
+        }
+        objIds.push(guild.id);
       }
-      objIds.push(guild.id);
-    }
-    user = (new User({
-      userId: profile.id,
-      discordTag: `${profile.username}#${profile.discriminator}`,
-      guilds: objIds,
-    })).save();
-  } else {
-    const userGuilds = profile.guilds.filter(async (g) => {
-      const guilds = await axios.get(`https://discord.com/api/v6/users/${process.env.CLIENT_ID}/guilds`, {
-        headers: {
-          Authorization: `BOT ${process.env.BOT_TOKEN}`,
-        },
+      user = (new User({
+        userId: profile.id,
+        discordTag: `${profile.username}#${profile.discriminator}`,
+        guilds: objIds,
+      })).save();
+    } else {
+      const userGuilds = profile.guilds.filter(async (g) => {
+        const guilds = await axios.get(`https://discord.com/api/v6/users/${process.env.CLIENT_ID}/guilds`, {
+          headers: {
+            Authorization: `BOT ${process.env.BOT_TOKEN}`,
+          },
+        });
+        if ((g.permissions & 32) === 32 && guilds.some((i) => i.id === g.id)) return true;
+        return false;
       });
-      if ((g.permissions & 32) === 32 && guilds.some((i) => i.id === g.id)) return true;
-      return false;
-    });
-    const objIds = [];
-    for (const g of userGuilds) {
-      // eslint-disable-next-line no-await-in-loop
-      let guild = await Guild.findOne({ guildId: g.id });
-      if (!guild) {
-        guild = (new Guild({
-          guildId: g.id,
-          prefix: '!',
-        })).save();
+      const objIds = [];
+      for (const g of userGuilds) {
+        // eslint-disable-next-line no-await-in-loop
+        let guild = await Guild.findOne({ guildId: g.id });
+        if (!guild) {
+          guild = (new Guild({
+            guildId: g.id,
+            prefix: '!',
+          })).save();
+        }
+        objIds.push(guild.id);
       }
-      objIds.push(guild.id);
+      User.findOneAndUpdate({ discordTag: `${profile.username}#${profile.discriminator}`, guilds: objIds });
     }
-    User.findOneAndUpdate({ discordTag: `${profile.username}#${profile.discriminator}`, guilds: objIds });
+    done(null, user);
+  } catch (e) {
+    done(e);
   }
-  done(user);
 }));
 
 module.exports = passport;
