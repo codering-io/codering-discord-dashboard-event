7dd649a66428103bb337e049ceb29ef18e1ecb2b
ToledoSDL <65132614+ToledoSDL@users.noreply.github.com>
2020-08-14 09:39:34 -0300

    Change all things for prefix be per guild

--- discord-bot/src/events/message/message.js
+++ discord-bot/src/events/message/message.js
@@ -1,21 +1,34 @@
-const BaseEvent = require('../../utils/structures/BaseEvent');
+const BaseEvent = require("../../utils/structures/BaseEvent");
+var { guild, createNewGuildEntry } = require("../../database/Guild");
 
 module.exports = class MessageEvent extends BaseEvent {
   constructor() {
-    super('message');
+    super("message");
   }
 
   async run(client, message) {
-    if (message.author.bot) return;
-    if (message.content.startsWith(client.prefix)) {
-      const [cmdName, ...cmdArgs] = message.content
-        .slice(client.prefix.length)
-        .trim()
-        .split(/\s+/);
-      const command = client.commands.get(cmdName);
-      if (command) {
-        command.run(client, message, cmdArgs);
+    guild.findById(message.guild.id, (error, guildTable) => {
+      if (error) {
+        console.log(error);
+        return 1;
       }
-    }
+      if (!guildTable) {
+        createNewGuildEntry(message.guild.id);
+        message.reply("Try again!");
+        return 1;
+      }
+      const prefix = guildTable.prefix;
+      if (message.author.bot) return;
+      if (message.content.startsWith(prefix)) {
+        const [cmdName, ...cmdArgs] = message.content
+          .slice(prefix.length)
+          .trim()
+          .split(/\s+/);
+        const command = client.commands.get(cmdName);
+        if (command) {
+          command.run(client, message, cmdArgs);
+        }
+      }
+    });
   }
 };
